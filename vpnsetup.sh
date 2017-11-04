6707efc8e9c8764b19d3533331a2ca3e23ae9e44
huang_mj@hotmail.com <huang_mj@hotmail.com>
2017-11-04 10:09:41 +0800

    add conf

--- vpnsetup.sh
+++ vpnsetup.sh
@@ -24,9 +24,9 @@
 # - All values MUST be placed inside 'single quotes'
 # - DO NOT use these characters within values:  \ " '
 
-YOUR_IPSEC_PSK=''
-YOUR_USERNAME=''
-YOUR_PASSWORD=''
+YOUR_IPSEC_PSK='myawsipsecvpnserver'
+YOUR_USERNAME='aws'
+YOUR_PASSWORD='awspasswd'
 
 # Important notes:   https://git.io/vpnnotes
 # Setup VPN clients: https://git.io/vpnclients
